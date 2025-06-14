01c1fc5168653d2dd54f42971b7e68fcf22a1108
Arjun Asok Nair <chelseaarjun@gmail.com>
2025-06-15 04:56:24 +0530

    updDate base url in vite config

--- flags-countries-game/vite.config.js
+++ flags-countries-game/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/flags-countries-game/',
+  base: '/kids-educational-games/flags-countries-game/',
   plugins: [react()],
 }); 
